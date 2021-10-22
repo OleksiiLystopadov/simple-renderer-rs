047efc2b535decc25fb73c18b7083cf092402aa5
Oleksii Lystopadov <alexey.listopadov@gmail.com>
2021-10-22 23:10:56 +0300

    Implement line drawing

--- README.md
+++ README.md
@@ -6,5 +6,11 @@ Learning project for earning rendering basics. The main goal is to create a very
 
 ## Learning Materials
 
+### Tutorials
 * https://habr.com/ru/post/248153/ - original tutorial.
+  
+### Theoretical Part
+* [Bresenham's line algorithm](https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm) - a line drawing algorithm that determines the points of an n-dimensional raster that should be selected in order to form a close approximation to a straight line between two points.
+
+### Similar Projects
 * https://github.com/RogovArtem/RustTry3d/tree/v1.1 - another rust implementation.
--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,20 @@ mod tga_writer;
 use tga_writer::Image;
 use tga_writer::Color;
 
+fn draw_line(x0: f32, y0: f32, x1: f32, y1: f32, color: Color, image: &mut Image) {
+    for step in 1..100 {
+        let normalized_step: f32 = step as f32 * 0.01;
+        let x = x0 * (1. - normalized_step) + x1 * normalized_step;
+        let y = y0 * (1. - normalized_step) + y1 * normalized_step;
+        image.set_pixel(x as i32, y as i32, color);
+    }
+}
+
 fn main() {
     let red: Color = Color::new(255, 0, 0);
 
     let mut image = Image::new(100, 100);
-    image.set_pixel(52, 41, red);
+
+    draw_line(10.0, 20.0, 80.0, 40.0, red, &mut image);
     image.write_to_tga("output.tga").unwrap();
 }
